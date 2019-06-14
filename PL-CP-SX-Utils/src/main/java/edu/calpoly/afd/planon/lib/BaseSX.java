4f4aaf2bd44677e933ab426b7afe0e11cf3febf4
Scott Tucker <srtucker@calpoly.edu>
2019-06-14 15:52:00 -0700

    added support to BaseSX fo function to check if running the SX on a valid BO type.

--- PL-CP-SX-Utils/src/main/java/edu/calpoly/afd/planon/lib/BaseSX.java
+++ PL-CP-SX-Utils/src/main/java/edu/calpoly/afd/planon/lib/BaseSX.java
@@ -2,9 +2,10 @@ package edu.calpoly.afd.planon.lib;
 
 import java.io.IOException;
 import java.util.Objects;
-
+import java.util.Arrays;
 import edu.calpoly.afd.planon.lib.VersionProvider;
 import edu.calpoly.afd.planon.lib.exception.PropertyNotDefined;
+import edu.calpoly.afd.planon.lib.exception.SXException;
 import nl.planon.hades.userextension.uxinterface.*;
 
 public abstract class BaseSX implements IUserExtension {
@@ -15,13 +16,15 @@ public abstract class BaseSX implements IUserExtension {
 	}
 	
 	protected abstract void execute(IUXBusinessObject newBO, IUXBusinessObject oldBO, IUXContext context, String parameters)
-			throws PropertyNotDefined, IOException;
+			throws PropertyNotDefined, SXException, IOException;
 	
 	public final void executeUX(IUXBusinessObject newBO, IUXBusinessObject oldBO, IUXContext context, String parameters) {
 		try {
 			this.execute(newBO, oldBO, context, parameters);
 		} catch (PropertyNotDefined e) {
 			context.addError(900, e.getMessage());
+		} catch (SXException e) {
+			e.toError(context);
 		} catch (IOException e) {
 			context.addError(900, e.getMessage());
 		}
@@ -37,5 +40,21 @@ public abstract class BaseSX implements IUserExtension {
 		return VersionProvider.getVersion() + " ["+ VersionProvider.getBuild() + "] - " + this.description;
 	}
 	
-	
+	public final void checkBOType(IUXBusinessObject bo, String... requiredBOTypeNames) throws SXException {
+		if (requiredBOTypeNames == null) {
+			throw new IllegalArgumentException("checkBOType: requiredBOTypeNames is required");
+		}
+		String boTypeName = bo.getTypeName();
+		String boSystemTypeName = bo.getBODefinition().getSystemBOType();
+		
+		boolean typeMatches = Arrays.asList(requiredBOTypeNames).contains(boTypeName);
+		
+		if(!boTypeName.equals(boSystemTypeName)) {
+			typeMatches = typeMatches || Arrays.asList(requiredBOTypeNames).contains(boSystemTypeName);
+		}
+		
+		if(!typeMatches) {
+			throw new SXException(1, "Invalid type for BO "+boTypeName);
+		}
+	}
 }
---
+++ PL-CP-SX-Utils/src/main/java/edu/calpoly/afd/planon/lib/exception/SXException.java
@@ -0,0 +1,26 @@
+package edu.calpoly.afd.planon.lib.exception;
+
+import nl.planon.hades.userextension.uxinterface.IUXContext;
+
+public class SXException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private final Integer errorCode;
+
+	public SXException(int errorCode, String message) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+	
+	public SXException(int errorCode, String message, Throwable cause) {
+		super(message, cause);
+		this.errorCode = errorCode;
+	}
+	
+	public final int getErrorCode() {
+		return this.errorCode;
+	}
+	
+	public final void toError(IUXContext context) {
+		context.addError(this.errorCode, this.getMessage());
+	}
+}
