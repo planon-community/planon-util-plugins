871ccaaead39fb24b47f3d98991c0dfeb52b882f
Scott Tucker <srtucker@calpoly.edu>
2020-05-06 12:57:06 -0700

    Added boolean properties to BaseProperties

--- PL-Base-Utils/src/main/java/edu/planon/lib/common/BaseProperties.java
+++ PL-Base-Utils/src/main/java/edu/planon/lib/common/BaseProperties.java
@@ -2,12 +2,16 @@ package edu.planon.lib.common;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 import edu.planon.lib.common.exception.PropertyNotDefined;
 
 public class BaseProperties extends Properties {
 	private static final long serialVersionUID = 1L;
+	public static final List<String> TRUE_VALUES = Collections.unmodifiableList(Arrays.asList("true", "t", "y", "yes", "1"));
 
 	public BaseProperties(String parameters) throws IOException {
 		this.load(new StringReader(parameters));
@@ -38,5 +42,15 @@ public class BaseProperties extends Properties {
 	public int getIntegerProperty(String key, int defaultValue) {
 		String propertyValue = this.getStringProperty(key, Integer.toString(defaultValue));
 		return Integer.parseInt(propertyValue);
+	}
+	
+	public boolean getBooleanProperty(String key) throws PropertyNotDefined {
+		this.assertKeyExists(key);
+		return this.getBooleanProperty(key, false);
+	}
+
+	public boolean getBooleanProperty(String key, boolean defaultValue) {
+		String propertyValue = this.getStringProperty(key, Boolean.toString(defaultValue));
+		return TRUE_VALUES.contains(propertyValue.toLowerCase());
 	}
 }
